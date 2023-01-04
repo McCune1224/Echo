3a953713c862e4f44ce7ad8420209c32cf293ba3
McCune1224 <alexmccune1224@gmail.com>
2023-01-04 00:08:48 -0800

    Svelte scuffed with typescript on Brian's box so we using React instead for the frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
-import postcss from './postcss.config.js';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  css:{
-    postcss
-  }
+  plugins: [react()]
 })
