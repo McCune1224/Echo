4be5247f61bd5f20b548f9406603767a5045ddcc
Brian <BrianMJK@Gmail.com>
2023-01-06 17:04:41 -0800

    YARN LOCk

--- frontend/src/components/Spotify.tsx
+++ frontend/src/components/Spotify.tsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 
-const SpotifyAuthentication = () => {
- 
+const SpotifyReponse = () => {
+
+}
+
+const SpotifyAuthentication = () => {  
+
     const [text, setText] = useState("Spotify Login");
 
-    const handleClick = () => {  
-            setText(text + " " + text);  
+    const handleClick = () => {    
+        var endpoint : string = "https://echo-backend-production.up.railway.app/oauth/spotify";
+        axios.get(endpoint)
+        .then(function (response) {
+          // handle success
+          return <div><button onClick={handleClick}>{response.status}</button></div>
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
       } 
 
     return <div><button onClick={handleClick}>{text}</button></div>
