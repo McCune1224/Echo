d35b64dc08a292466748e777a1e469a13d70b869
McCune1224 <alexmccune1224@gmail.com>
2023-01-08 19:43:19 -0800

    Removed postgre testing

--- backend/main.go
+++ backend/main.go
@@ -26,9 +26,10 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		// Frontend domains
-		AllowOrigins: "http://localhost:3000, https://echo-frontend.up.railway.app/",
-		AllowHeaders: "Origin, Content-Type, Accept",
+		// Frontend domains whitelist to allow to pass credentials
+		AllowOrigins:     "http://localhost:3000, https://echo-frontend.up.railway.app/",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowCredentials: true,
 	}))
 
 	// Grouping routes
