6c627e1263e84b947906f76533f7ba57a213f30c
Brian <BrianMJK@Gmail.com>
2023-01-15 17:12:23 -0800

    fuckery

---
+++ frontend/src/lib/Auth.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    var state = false;
+
+    function OnClick()
+    {
+        state = !state;
+        console.log(state);
+    }
+
+
+</script>
+
+<div> 
+
+</div>
+
+<div>
+    
+</div>
+
+<button type="button" on:click={OnClick}>My Button</button>
