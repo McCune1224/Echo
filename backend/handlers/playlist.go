822b48054bee072f3cd2a9d5f224324392b5e571
McCune1224 <alexmccune1224@gmail.com>
2023-01-16 23:47:57 -0800

    OOPSIE WOOPSIE BAD DEVY WEVY

--- backend/handlers/playlist.go
+++ backend/handlers/playlist.go
@@ -1,25 +1,11 @@
 package handlers
 
 import (
-	"github.com/McCune1224/Echo/models"
-	"github.com/McCune1224/Echo/repository"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetAllPlaylists(c *fiber.Ctx) error {
-	// Get the userToken from the JWT
-	userToken := c.Locals("user").(*models.User)
-	db := repository.DBConnection
-	var playlists []models.Playlist
-
-	// Get all playlists for the user
-	db.Where("user_id = ?", userToken.ID).Find(&playlists)
-
-	// Convert the Playlist struct to PlaylistResponse struct
-	var playlistResponses []models.PlaylistResponse
-	for _, playlist := range playlists {
-		playlistResponses = append(playlistResponses, models.PlaylistResponse{
-			Name: playlist.Name,
-		})
-	}
+	return c.JSON(fiber.Map{
+		"message": "OMOAIWA MOSHINDARU",
+	})
 }
